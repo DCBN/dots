b0d363ab7ff856336d8d853f2699fd2f10ff53c0
skr0tm <skr0tm@protonmail.com>
2016-03-21 11:32:32 +0100

    cleaned up the repo a bit

---
+++ lock.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+declare icon tempbg
+icon="$HOME/.xlock/icon.png"
+tmpbg='/tmp/screen.png'
+
+finish() {
+  [[ -f "$tmpbg" ]] && rm "$tmpbg"
+}
+
+trap finish SIGHUP SIGINT SIGTERM
+
+(( $# )) && icon="$1"
+
+scrot "$tmpbg"
+
+convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
+convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+
+i3lock -u -i "$tmpbg"
+
+finish
+
